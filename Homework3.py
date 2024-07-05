71016c1390411213b4abf227af20d3b1c7dd26b4
kerimovmurat <kerimovmurat@yandex.ru>
2024-07-05 15:45:23 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,9 @@
+name = "Name:Murat"
+print(name, sep='')
+age = 37
+print('Age:', age, sep='')
+age = 37+2
+print('New age:', age, sep='')
+is_student = True
+print('Is student:', is_student, sep='')
+
