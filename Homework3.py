1b9bfc454bc301b42c6f8ad7685771ae96b24446
kerimovmurat <kerimovmurat@yandex.ru>
2024-07-06 10:31:19 +0300

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -4,6 +4,6 @@ age = 37
 print('Age:', age, sep='')
 age = 37+2
 print('New age:', age, sep='')
-is_student = True
+is_student = (age >= 37 or age = 37+2)
 print('Is student:', is_student, sep='')
 
